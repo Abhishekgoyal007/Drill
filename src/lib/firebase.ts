76dfd2645403be419ebdd877e34b56383aacce6d
Abhishekgoyal007 <abhishekgoyal1311@gmail.com>
2026-04-02 12:48:12 +0530

    fix: move Firebase config to env variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBGVzyeAgxT3b7lQSul7OsFrm1iUsc9SnM",
-  authDomain: "drill-57978.firebaseapp.com",
-  projectId: "drill-57978",
-  storageBucket: "drill-57978.firebasestorage.app",
-  messagingSenderId: "573652458576",
-  appId: "1:573652458576:web:3b345643ec9312bf95c555",
-  measurementId: "G-ZSKETCVK5J"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase (prevent double-init in dev mode)
